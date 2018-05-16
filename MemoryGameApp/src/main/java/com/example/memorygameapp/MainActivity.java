13f1509cf8cb0ce71b6ec810fde18b4c2934b9a4
Konstantin <twardzk@hochschule-trier.de>
2018-05-16 16:00:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- MemoryGameApp/src/main/java/com/example/memorygameapp/MainActivity.java
+++ MemoryGameApp/src/main/java/com/example/memorygameapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.memorygameapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
